b35197047761563b1593cde7d7e521f3943dd234
robgoyal <robin_goyal@hotmail.com>
2016-06-22 07:24:16 -0400

    Added exercise 3

---
+++ Python/ex3/ex3_firstex.py
@@ -0,0 +1,33 @@
+# prints the following line
+print "I will now count my chickens:"
+
+# prints the line and calculates the number of Hens
+print "Hens", 25 + 30 / 6
+
+# prints the line and calculates the number of roosters
+print "Roosters", 100 - 25 * 3 % 4
+
+#prints a line
+print "Now I will count the eggs:"
+
+# calculates the number of eggs
+print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 +6
+
+# prints the line
+print "Is it true that 3 + 2 < 5 - 7?"
+
+# prints the result of the expression
+print 3 + 2 < 5 - 7
+
+# following two lines print the result of the expressions
+print "What is 3 + 2?", 3 + 2
+print "What is 5 - 7?", 5 - 7
+
+# prints the lines
+print "Oh, that's why it's False."
+print "How about some more."
+
+# prints the lines and expressions
+print "Is it greater?", 5 > -2
+print "Is it greater or equal?", 5 >= -2
+print "Is it less or equal?", 5 <= -2
---
+++ Python/ex3/ex3_secondex.py
@@ -0,0 +1,7 @@
+# I want to find out what 35 degrees Celsius is in Fahrenheit
+
+temperature = 35
+
+# Celsius to Fahrenheit calculation
+fahrenheit_temp = 35 * (9.0/5) + 32
+print "35 degrees Celsius in Fahrenheit is: ",  fahrenheit_temp
