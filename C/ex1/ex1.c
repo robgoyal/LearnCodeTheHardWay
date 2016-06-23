08d57838e04363493f2eb8a1b3ac02ce294e4305
robgoyal <robin_goyal@hotmail.com>
2016-06-22 23:21:32 -0400

    Added C directory and Exercise 1

---
+++ C/ex1/ex1.c
@@ -0,0 +1,9 @@
+/* Dust Off That Compiler */
+
+#include <stdio.h>
+
+int main (int argc, char *argv[])
+{
+    puts("Hello world.");
+    return 0;
+}
---
+++ C/ex1/ex1_firstex.c
@@ -0,0 +1,19 @@
+/* Dust Off That Compiler */
+
+#include <stdio.h>
+
+int main (int argc, char *argv[])
+{
+    puts("Hello World.");
+    puts("This is my first C program");
+    puts("This is pretty cool");
+    puts("My name is Robin");
+    puts("What is your name?");
+    
+    return 0;
+}
+
+/* Removing the line "return 0;" does not affect the output of the program */
+/* Removing the line "#include <stdio.h> causes an error since puts is a function from that header file */
+/* Removing the data types such as "int main" or "int argc" causes an error of the return data type */ 
+
