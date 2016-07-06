15b0d7341888e4730952824fb6f116250fbf4d7e
robgoyal <robin_goyal@hotmail.com>
2016-07-06 13:44:19 -0400

    Added exercise 12

---
+++ Python/ex12/ex12.py
@@ -0,0 +1,11 @@
+#!/bin/python2
+
+# Prompting People
+## An updated version of ex11 with writing the string inside the raw_input prompt
+
+age = raw_input("How old are you? ")
+height = raw_input("How tall are you? ")
+weight = raw_input("How much do you weight? ")
+
+print "So, you're %r old, %r tall and %r heavy." % (
+        age, height, weight)
