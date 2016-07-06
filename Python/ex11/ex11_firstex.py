f27573cf567bc7b10f8f5dc68e15eefb5abbb895
robgoyal <robin_goyal@hotmail.com>
2016-07-06 12:00:10 -0400

    Added exercise 11

---
+++ Python/ex11/ex11_firstex.py
@@ -0,0 +1,12 @@
+#!/bin/python2
+# Asking Questions
+
+# An exercise to learn more about you
+
+print "What school do you go to?",
+school = raw_input()
+city = raw_input("What city do you live in? ")
+
+print "So you go to %r in %r" % (school, city)
+
+
