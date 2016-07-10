d8175f28abee590e832ea3fae4a314e678a0d9e1
robgoyal <robin_goyal@hotmail.com>
2016-07-10 11:48:41 -0400

    Added exercise 28

---
+++ Python/ex28/ex28.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env/python
+
+# Boolean Practice
+# Code written by Robin Goyal
+# Created on July 9, 2016
+# Last updated on July 9, 2016
+
+# An exercise to practice Boolean logic, my answers are written in comments on the right
+
+print True and True # True
+print False and True # False
+print 1 == 1 and 2 == 1 # False
+print "test" == "test" # True
+print 1 == 1 or 2 != 1 # True
+print True and 1 == 1 # True
+print False and 0 != 0 # False
+print True or 1 == 1 # True
+print "test" == "testing" # False
+print 1 != 0 and 2 == 1 # False
+print "test" != "testing" # True
+print "test" == 1 # False
+print not(True and False) # True
+print not(1 == 1 and 0 != 1) # False
+print not(10 == 1 or 1000 == 1000) # False
+print not(1 != 10 or 3 == 4) # False
+print not("testing" == "testing" and "Zed" == "Cool Guy") # True
+print 1 == 1 and (not("testing" == 1 or 1 == 0)) # True
+print "chunky" == "bacon" and (not(3==4 or 3==3)) # False
+print 3 == 3 and (not("testing" == "testing" or "Python" == "Fun")) # False
